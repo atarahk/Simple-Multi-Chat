3e43625779e114b27f20bd7a3fee5b240864421e
Atarah <atarahplayground@gmail.com>
2019-06-05 14:07:24 +0800

    multichat

--- public/chat.js
+++ public/chat.js
@@ -28,9 +28,6 @@ socket.on('chat', data => {
     '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
 })
 
-// socket.on('typing', data => {
-//   feedback.innerHTML = '<p><em>' + data + 'is typing message...</em></p>'
-// })
-socket.on('typing', function(data) {
-  feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>'
+socket.on('typing', data => {
+  feedback.innerHTML = '<p><em>' + data + 'is typing message...</em></p>'
 })
